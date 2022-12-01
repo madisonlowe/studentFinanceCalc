14f1f98bcbd7f6404202256cc82c71dec93d3eac
madisonlowe <mcourteney@live.com>
2022-12-01 15:58:21 +0000

    File edit.

---
+++ studentFinanceCalc/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace studentFinanceCalc
+{
+    class Program
+    {
+        static void Main()
+        {
+            // goal: calculate tax that will be taken off of you for uk student loans
+
+            // variables we need from user: annual wage, monthly wage, when you graduated, where you're from
+            // plan 1: current interest rate (changes regularly), threshhold for repayment
+            // plan 2: current interest rate (changes regularly), threshhold for repayment
+            // plan 4: current interest rate (changes regularly), threshhold for repayment
+            // masters: current interest rate (changes regularly), threshhold for repayment
+
+            //
+
+            // calculate for plan 1, plan 2, plan 4, and masters repayments
+
+            // take in: when they graduated, what from, how many degrees
+            // take in: will you be entering your wage hourly or annually
+            // - if annual: take in annual wage pre-tax
+            // - if hourly: take in hours worked monthly,
+            //   calculate monthly wage and then annual wage,
+            //   show back to user and ask if it seems correct before proceeding
+
+            // should also calculate general tax someone will pay to show net result?
+            Console.Write("am i working");
+        }
+    }
+}
